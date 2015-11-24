6ad14b97865283a4f5651e893884966e04f5e99b
Hannnah Roe <hannahdroe@gmail.com>
2015-11-24 15:59:42 -0700

    Styled hamburger and slide in nav

--- js/app.js
+++ js/app.js
@@ -7,9 +7,7 @@ app.controller("navCtrl", function($scope, $rootScope) {
 
     $scope.leftVisible = false;
     
-    $scope.close = function() {
-        $scope.leftVisible = false;
-    };
+   
 
     $scope.showLeft = function(e) {
         $scope.leftVisible = true;
@@ -20,7 +18,7 @@ app.controller("navCtrl", function($scope, $rootScope) {
 
     function _close() {
         $scope.$apply(function() {
-            $scope.close(); 
+            $scope.leftVisible = false; 
         });
     }
 
