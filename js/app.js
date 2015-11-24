2687c134592df9b3c7ba3fd3ceb939b7035ae60e
Hannnah Roe <hannahdroe@gmail.com>
2015-11-24 14:04:33 -0700

    Added in a push nav in for when hamburger is clicked in mobile view. Need to clean up css

---
+++ js/app.js
@@ -0,0 +1,52 @@
+var app = angular.module("NavApp", []);
+
+
+
+
+app.controller("navCtrl", function($scope, $rootScope) {
+
+    $scope.leftVisible = false;
+    
+    $scope.close = function() {
+        $scope.leftVisible = false;
+    };
+
+    $scope.showLeft = function(e) {
+        $scope.leftVisible = true;
+        e.stopPropagation();
+    };
+
+    
+
+    function _close() {
+        $scope.$apply(function() {
+            $scope.close(); 
+        });
+    }
+
+    $rootScope.$on("documentClicked", _close);
+    $rootScope.$on("escapePressed", _close);
+});
+
+app.directive("navLinks", function() {
+    return {
+        restrict: "C",
+        template: "<div ng-class='{ show: visible, left: alignment === \"left\" }' ng-transclude></div>",
+        transclude: true,
+        scope: {
+            visible: "=",
+            alignment: "@"
+        }
+    };
+});
+
+app.run(function($rootScope) {
+    document.addEventListener("keyup", function(e) {
+        if (e.keyCode === 27)
+            $rootScope.$broadcast("escapePressed", e.target);
+    });
+
+    document.addEventListener("click", function(e) {
+        $rootScope.$broadcast("documentClicked", e.target);
+    });
+});
