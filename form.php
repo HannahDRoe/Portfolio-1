b4034c012c7fc5aa41b65f69a73d5019e7214945
Hannah Roe <hannahdroe@gmail.com>
2016-02-19 21:44:18 -0700

    Updated layout for portfolio session. More work on php-form.

--- form.php
+++ form.php
@@ -2,6 +2,9 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-
+$recipient = "hannahdroe@gmail.com";
+$formContent = "From: $name \n Message: $message";
+$subject = "Contact Form";
+mail($recipient, $formContent, $subject, $message);
 
 ?>
