c257bd8b3ebe7bc8f257cd27efd9be1365993d14
Hannah Roe <hannahdroe@gmail.com>
2016-02-24 19:00:34 -0700

    Working on media queries and positioning.

--- form.php
+++ form.php
@@ -3,8 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $recipient = "hannahdroe@gmail.com";
-$formContent = "From: $name \n Message: $message";
 $subject = "Contact Form";
-mail($recipient, $formContent, $subject, $message);
+mail($recipient, $subject, $message);
 
 ?>
