07d78e3944d35fb08fc3039dc1ab9526fa0b6f55
Hannah Roe <hannahdroe@gmail.com>
2016-02-29 17:27:07 -0700

    Going to go with freespree.io for handling mail. Styling warning messages for invalid inputs on contact from.

--- form.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$recipient = "hannahdroe@gmail.com";
-$subject = "Contact Form";
-mail($recipient, $subject, $message);
-
-?>
